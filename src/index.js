e23fd36ba0aab8868c04327986d2240b9fc0c0f4
lavanya-vanwyk <lavanyavanwyk1@gmail.com>
2024-12-08 16:07:37 +0200

    Added loading animation.
    Edited AI API context string.
    
    Added an examples element for user friendliness.
    
    Added a loading animation through CSS and JS.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,23 @@ function generatePoem (event) {
     let apiKey ="2ec5a180t946o0ff04daaefa18395bb9";
     let userInput = document.querySelector("#form-input");
     let prompt = `User instructions: Generate a poem about ${userInput.value}`;
-    let context = "You are a digital bard capable of crafting verses in any style in English, from whimsical to profound. Please limit poem length to 14 lines, in basic HTML and separate each line with a <br />. Do not include a title to the poem. Sign the poem with 'by SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning."
+    let context = "You are a digital bard capable of crafting verses in any style in English, from whimsical to profound. Please limit poem length to 14 lines, in basic HTML and separate each line with a <br />. Do not include a title to the poem. Sign the poem with 'by SheCodes AI' inside a <strong> element at the end of the poem."
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
     let poemElement = document.querySelector("#poem-element");
     poemElement.classList.remove("hidden");
-    poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${userInput.value}</div>`;
+    poemElement.innerHTML = 
+    `<div class="generating-container">
+        <div class="loader-container">
+            <div class="loader-wrap">
+                <div class="loader">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </div>`;
 
     axios.get(apiURL).then(displayPoem);
 }
